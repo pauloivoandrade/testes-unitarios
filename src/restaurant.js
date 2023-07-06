e330c80aa9c68a216fdc9861bc2ccff1f7bd400c
pauloivoandrade <pauloivoasantos@gmail.com>
2023-07-05 23:31:57 -0300

    feat: function createMenu e tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,6 +1,40 @@
 /* eslint-disable max-len */
 // Siga as orientações do README!
 
-const createMenu = () => {};
-
+const createMenu = (menuData) => {
+    if (!menuData) {
+      throw new Error('Menu não fornecido');
+    }
+  
+    const menu = {
+      fetchMenu: () => menuData,
+      consumption: [],
+      order(item) {
+        const { food, drinks } = this.fetchMenu();
+        if (Object.prototype.hasOwnProperty.call(food, item) || Object.prototype.hasOwnProperty.call(drinks, item)) {
+          this.consumption.push(item);
+        } else {
+          return 'Item indisponível';
+        }
+      },
+      pay() {
+        const { food, drinks } = this.fetchMenu();
+        const totalPrice = this.consumption.reduce((total, item) => {
+          const price = food[item] || drinks[item] || 0;
+          return total + price;
+        }, 0);
+        return totalPrice * 1.1;
+      },
+    };
+  
+    return menu;
+  };
 module.exports = createMenu;
+
+// const createStudent = (name) => {
+//     const estudante = {};
+//     const frase = 'Eita pessoa boa!';
+//     estudante.name = name;
+//     estudante.feedback = () => frase;
+//     return estudante;
+//     };
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -2,7 +2,22 @@ const createMenu = require('../src/restaurant');
  
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
   it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // Escreva todos os testes aqui.
-  });
+    expect(() => createMenu()).toThrowError();
+  });  
+  it('A função createMenu() retorna um objeto que possui a chave fetchMenu', () => {
+    expect(createMenu({ food: {}, drinks: {} })).toHaveProperty('fetchMenu');
+  })
+  it('Verifica se o valor da chave fetchMenu do objeto retornado pela função createMenu() é uma função', () => {
+    expect(typeof createMenu({ food: {}, drinks: {} }).fetchMenu).toBe('function');
+  })
+  it('Verifica se o objeto retornado pela função createMenu({ food: {}, drinks: {} }).fetchMenu() retorna um objeto cujas chaves são somente food e drinks', () => {
+    const menu = createMenu({ food: {}, drinks: {} }).fetchMenu();
+    expect(menu).toEqual({ food: {}, drinks: {} });
+  });  
+  it('Verifica se o menu passado para a função createMenu() é idêntico ao menu recuperado pela função createMenu({ food: {}, drinks: {} }).fetchMenu()', () => {
+    const menuData = { food: {}, drinks: {} };
+    const menu = createMenu(menuData);
+    expect(menu.fetchMenu()).toEqual(menuData);
+  });  
 });
+ 
